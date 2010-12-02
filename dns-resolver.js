b46c9defca0f65cfaa7bdba0e132a64e2b629448
Lothar Braun <braun@net.in.tum.de>
2010-12-02 10:46:39 +0100

    check for nameserver

--- dns-resolver.js
+++ dns-resolver.js
@@ -25,36 +25,53 @@ function add_new_domain(domain) {
 }
 
 function lookup_domain(domain) {
-	dns.resolve4(domain, function (err, addresses, ttl) {
-		// TODO: better error handling!
+	dns.resolve(domain, rrtype='NS', function (err, addresses) {
 		if (err) {
-			console.log('Error locking up domain: ' + JSON.stringify(err));
-			// try again in an hour!
-			setTimeout(lookup_domain, 3600 * 1000, domain);
+			console.log('Error looking up nameserver for "' + domain + '": ' + JSON.stringify(err));
+			// TODO: or should we scheulde the new lookup??
+			//setTimeout(lookup_domain, 3600 * 1000, domain);
 			return;
 		}
-		dns_resolver_finished(domain, addresses, ttl);
+		dns_resolver_finished('NS', domain, addresses, 0);
+	});
+
+	dns.resolve(domain, rrtype='A', function (err, addresses, ttl) {
+		// TODO: better error handling!
+		if (err) {
+			console.log('Error locking up "' + domain + '": ' + JSON.stringify(err));
+			if (ttl == 0) {
+				ttl = 3600 * 1000;
+			}
+			setTimeout(lookup_domain, ttl, domain);
+			return;
+		}
+		console.log("Got TTL: " + ttl);
+		dns_resolver_finished('A', domain, addresses, ttl);
+		if (ttl == 0) {
+			ttl = 1
+		}
+		
+		//console
+		setTimeout(lookup_domain, ttl * 1000, domain);
 	});
 }
 
-function dns_resolver_finished(domain, addresses, ttl) {
-
+function dns_resolver_finished(type, domain, addresses, ttl) {
 	var timestamp = new Date();
 
-	console.log("Got resolution for " + domain + ". Rescheduling query in " + ttl + " seconds");
+	console.log("Got " + type + " resolution for " + domain + ". Rescheduling query in " + ttl + " seconds");
 
 	fs.open(config.data_dir + '/' + domain, "a+", 0666, function (err, fd) {
 		if (err) throw err;
 		//console.log("Opened file: " + JSON.stringify(stat));
 		addresses.forEach(function (a) {
-			var line = domain + " " + ttl + " " + a + " " + timestamp.getTime() + "\n";
+			var line = type + " " + domain + " " + ttl + " " + a + " " + timestamp.getTime() + "\n";
 			fs.write(fd, line, null, encoding='utf8', function (err, written) {
 				if (err) throw err;
 			});
 		});
 	});
 	
-	setTimeout(lookup_domain, ttl * 1000, domain);
 };
 
 
