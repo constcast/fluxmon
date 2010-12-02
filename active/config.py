cb8557643ad51f54db18a11cf23f3be513bb63eb
Lothar Braun <braun@net.in.tum.de>
2010-12-02 11:52:58 +0100

    complete startup code

--- active/config.py
+++ active/config.py
@@ -1,1 +1,3 @@
 storage_dir="fluxmon-data/"
+domain_db ="fluxdomains.txt.db"
+new_domain_file="new_domains.txt"
--- active/fluxmon.py
+++ active/fluxmon.py
@@ -2,12 +2,20 @@
 
 import os, sys
 
-import config
+import config, domain_agent
+
+def pull_domains_from_db(db):
+	pass
 
 if __name__ == "__main__":
 	print "Starting up fluxmon ..."
 	if (os.path.isdir(config.storage_dir)) == True:
-		print "Found Dir"
+		print "Found data storage directory \"" + config.storage_dir + "\"...";
+		domain_db = config.storage_dir + "/" + config.domain_db;
+		if (os.path.isfile(domain_db)):
+			print "Found existing domain database. Reading domains ....";
+			initial_domain_list = pull_domains_from_db(domain_db);
+			
 	else:
 		print "Directory ", config.storage_dir, " does not exist! Tyring to create the directory ..."
 		try: 
@@ -15,3 +23,6 @@ if __name__ == "__main__":
 		except OSError as e:
 			print "Cannot create directory \"", config.storage_dir, "\": ", e.args;
 			sys.exit(-1);
+
+	agent = domain_agent.DomainAgent(initial_domain_list, config.new_domain_list)
+	agent.run()
